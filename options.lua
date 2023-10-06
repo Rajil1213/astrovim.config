09b9e9fb3b87960c35c82e9f3d74a239cb42ebe1
Rajil Bajracharya <imrb1213@gmail.com>
2023-10-06 12:17:34 +0545

    fix: set cc to 100

--- options.lua
+++ options.lua
@@ -7,7 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = true, -- sets vim.opt.wrap
-    colorcolumn = "120",
+    colorcolumn = "100", -- for Rust, TS/JS
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
