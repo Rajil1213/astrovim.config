fae9ece779e6c4a886d1386454a60ae36d4a69a2
Rajil Bajracharya <imrb1213@gmail.com>
2023-10-12 13:58:10 +0545

    refactor: remove formatting from tsserver
    Use null-ls (prettierd, eslint_d) instead

--- init.lua
+++ init.lua
@@ -41,6 +41,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "tsserver",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -72,10 +73,11 @@ return {
               includeInlayParameterNameHintsWhenArgumentMatchesName = false,
               includeInlayPropertyDeclarationTypeHints = true,
               includeInlayVariableTypeHints = false,
+              auto = false,
             },
             format = {
               insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = false,
-              insertSpaceBeforeFunctionParenthesis = false,
+              insertSpaceBeforeFunctionParenthesis = true,
             },
           },
 
@@ -88,10 +90,11 @@ return {
               includeInlayParameterNameHintsWhenArgumentMatchesName = false,
               includeInlayPropertyDeclarationTypeHints = true,
               includeInlayVariableTypeHints = false,
+              auto = false,
             },
             format = {
               insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = false,
-              insertSpaceBeforeFunctionParenthesis = false,
+              insertSpaceBeforeFunctionParenthesis = true,
             },
           },
         },
